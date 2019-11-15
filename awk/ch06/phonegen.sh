2e5718470781df0bd19568213c611e669873d27a
Meelis Nigols <meelis.nigols@telia.ee>
2019-11-15 12:16:30 +0200

    replaced second awk with grep

--- awk/ch06/phonegen.sh
+++ awk/ch06/phonegen.sh
@@ -6,6 +6,4 @@ BEGIN{
     srand()
     for (x=1;x<1000;x++)
         printf("%3d-%3d-%4d\n",rand()*1000,rand()*1000,rand()*10000)
-} ' | awk '
-/[[:digit:]]{3}-[0-9]{3}-[0-9]{4}/
-' | sort -u > numbers
+} ' | grep -E '[[:digit:]]{3}-[0-9]{3}-[0-9]{4}' | sort -u > numbers
